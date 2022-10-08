fa935b700df5520ed5553a3c9ea8304390f3e9d4
Sergey <SergeySm128@gmail.com>
2022-10-08 12:17:12 +0300

    add new version

---
+++ app.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        centerMode: true,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: true,
+        dots: false,
+        pauseOnHover: true,
+        responsive: [
+            {
+                breakpoint: 1199,
+                settings: {
+                    arrows: true,
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: true,
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    arrows: true,
+                    centerMode: false,
+                    slidesToShow: 2
+                }
+            },
+        ]
+    });
+    // const qr_button = $('.banner .link_app_store_qr');
+    // if($( window ).width() > 767) {
+    //     // qr_button.click();
+    // } else {
+    //     qr_button.hide();
+    // }
+
+
+    // let block_about = $('.block-about');
+    // setInterval(function () {
+    //     block_about.toggleClass('active')
+    // }, 800);
+
+    // let bg_iphone = $('.bg_iphone');
+    // let block_benefits_info__img = $('.block-benefits_info__img');
+    // setInterval(function () {
+    //     bg_iphone.toggleClass('active')
+    //     block_benefits_info__img.toggleClass('active')
+    // }, 10000);
+
+});
