cc0149777da0eb3031f416031b603b1c3b71d6c6
Sergey <SergeySm128@gmail.com>
2023-03-25 16:00:46 +0200

    add page privacy

--- src/i18n/es.js
+++ src/i18n/es.js
@@ -27,6 +27,90 @@ const translate = {
         content_8: "Las suscripciones pueden ser gestionadas por el usuario y la renovación automática puede ser desactivada yendo a Configuración de cuenta de iTunes del usuario después de la compra.",
         content_9: "No se permite la cancelación de la suscripción actual durante el período de suscripción activo.",
         content_10: "Puedes desactivar la renovación automática de tu suscripción a través de Configuración de cuenta de iTunes. Sin embargo, no podrás cancelar la suscripción actual durante su período activo."
+    },
+    page_privacy: {
+        top_text_block: "Fecha de entrada en vigencia: 3 de octubre de 2022<br><br>\n" +
+            "Todas las aplicaciones (\"nosotros\", \"nos\" o \"nuestro\") operan la aplicación móvil de Todas las aplicaciones (el \"Servicio\"). Esta página le informa sobre nuestras políticas con respecto a la recolección, uso y divulgación de datos personales cuando utiliza nuestro Servicio y las opciones que tiene asociadas con esos datos",
+        middle_content_rubric_1: "Nuestra Política de Privacidad para All apps ha sido creada con la ayuda del sitio web Free Privacy Policy.",
+        middle_content_rubric_2: "<p>Utilizamos sus datos para proporcionar y mejorar el Servicio. Al utilizar el Servicio, acepta la recopilación y uso de información de acuerdo con esta política. A menos que se defina de otra manera en esta Política de Privacidad, los términos utilizados en esta Política de Privacidad tienen los mismos significados que en nuestros Términos y Condiciones.</p>\n" +
+            "<p>Recopilamos varios tipos diferentes de información para diversos fines con el fin de proporcionar y mejorar nuestro Servicio para usted.</p>",
+        middle_content_rubric_3: "<strong>Datos personales</strong>\n" +
+            "<p>Al utilizar nuestro Servicio, es posible que le solicitemos que nos proporcione cierta información de identificación personal que se puede utilizar para contactarlo o identificarlo (\"Datos personales\"). La información de identificación personal puede incluir, entre otros, los siguientes datos:</p>\n" +
+            "<ul><li>Cookies y datos de uso</li></ul>",
+        middle_content_rubric_4: "<strong>Datos de Uso</strong>\n" +
+            "<p>Cuando accedes al Servicio a través de un dispositivo móvil, podemos recopilar cierta información automáticamente, incluyendo, pero no limitándose a, el tipo de dispositivo móvil que utilizas, la identificación única de tu dispositivo móvil, la dirección IP de tu dispositivo móvil, tu sistema operativo móvil, el tipo de navegador de Internet móvil que utilizas, identificadores únicos de dispositivo y otros datos de diagnóstico (\"Datos de Uso\").</p>",
+        middle_content_rubric_5: "<strong>Seguimiento y Datos de Cookies</strong>\n" +
+            "<p>Utilizamos cookies y tecnologías de seguimiento similares para rastrear la actividad en nuestro Servicio y mantener cierta información.</p>\n" +
+            "<p>Las cookies son archivos con una pequeña cantidad de datos que pueden incluir un identificador único anónimo. Las cookies se envían a tu navegador desde un sitio web y se almacenan en tu dispositivo. Las tecnologías de seguimiento también utilizadas son balizas, etiquetas y scripts para recopilar y rastrear información y para mejorar y analizar nuestro Servicio.</p>\n" +
+            "<p>Puedes instruir a tu navegador para que rechace todas las cookies o para que te avise cuando se envía una cookie. Sin embargo, si no aceptas las cookies, es posible que no puedas utilizar algunas partes de nuestro Servicio.</p>",
+        middle_content_rubric_6: "<strong>Ejemplos de Cookies que usamos:</strong>\n" +
+            "<ul>\n" +
+            "<li>Cookies de sesión. Utilizamos cookies de sesión para operar nuestro servicio;</li>\n" +
+            "<li>Cookies de preferencia. Utilizamos cookies de preferencia para recordar sus preferencias y varias configuraciones;</li>\n" +
+            "<li>Cookies de seguridad. Utilizamos cookies de seguridad con fines de seguridad.</li>\n" +
+            "</ul>",
+        middle_content_rubric_7: "<strong>Todos apps utiliza los datos recopilados para diversos fines:</strong>\n" +
+            "<ul>\n" +
+            "<li>Para proporcionar y mantener el Servicio;</li>\n" +
+            "<li>Para notificarle sobre cambios en nuestro Servicio;</li>\n" +
+            "<li>Para permitirle participar en funciones interactivas de nuestro Servicio cuando decida hacerlo;</li>\n" +
+            "<li>Para proporcionar atención al cliente y soporte;</li>\n" +
+            "<li>Para proporcionar análisis o información valiosa para que podamos mejorar el Servicio;</li>\n" +
+            "<li>Para monitorear el uso del Servicio;</li>\n" +
+            "<li>Para detectar, prevenir y abordar problemas técnicos.</li>\n" +
+            "</ul>",
+        middle_content_rubric_8: "<strong>Transferencia de datos</strong>\n" +
+            "<p>Su información, incluyendo Datos personales, puede ser transferida y mantenida en computadoras ubicadas fuera de su estado, provincia, país u otra jurisdicción gubernamental donde las leyes de protección de datos pueden ser diferentes de las de su jurisdicción.</p>\n" +
+            "<p>Su consentimiento a esta Política de privacidad, seguido de su envío de dicha información, representa su acuerdo con esa transferencia.</p>\n" +
+            "<p>All apps tomará todas las medidas razonablemente necesarias para asegurarse de que sus datos sean tratados de manera segura y de acuerdo con esta Política de privacidad y ninguna transferencia de sus Datos personales se llevará a cabo a una organización o un país a menos que haya controles adecuados en su lugar, incluyendo la seguridad de sus datos y otra información personal.</p>",
+        middle_content_rubric_9: "<strong>Divulgación de datos</strong>\n" +
+            "<span>Requisitos legales</span>\n" +
+            "<p>Todos apps puede divulgar su información de identificación personal en la creencia de buena fe de que dicha acción es necesaria para:</p>\n" +
+            "<ul>\n" +
+            "<li>Cumplir con una obligación legal;</li>\n" +
+            "<li>Proteger y defender los derechos o la propiedad de All apps;</li>\n" +
+            "<li>Prevenir o investigar posibles infracciones en relación con el Servicio;</li>\n" +
+            "<li>Proteger la seguridad personal de los usuarios del Servicio o del público;</li>\n" +
+            "<li>Proteger contra la responsabilidad legal.</li>\n" +
+            "</ul>",
+        middle_content_rubric_10: "<strong>Seguridad de los Datos</strong>\n" +
+            "<p>La seguridad de tus datos es importante para nosotros, pero recuerda que ningún método de transmisión por Internet o de almacenamiento electrónico es 100% seguro. Si bien nos esforzamos por utilizar medios comercialmente aceptables para proteger tus datos personales, no podemos garantizar su seguridad absoluta.</p>",
+        middle_content_rubric_11: "<p>Podemos emplear a empresas e individuos externos para facilitar nuestro servicio (\"Proveedores de servicio\"), para proporcionar el servicio en nuestro nombre, para realizar servicios relacionados con el servicio o para ayudarnos a analizar cómo se utiliza nuestro servicio.</p>\n" +
+            "<p>Estos terceros tienen acceso a sus datos personales solo para realizar estas tareas en nuestro nombre y están obligados a no divulgarlos ni utilizarlos para ningún otro fin.\n</p>",
+        middle_content_rubric_12: "<strong>Análisis</strong>\n" +
+            "<p>Podemos utilizar proveedores de servicios de terceros para supervisar y analizar el uso de nuestro servicio.</p>",
+        middle_content_rubric_13: "<strong>Google Analytics</strong>\n" +
+            "<p>Google Analytics es un servicio de análisis web ofrecido por Google que rastrea e informa sobre el tráfico del sitio web. Google utiliza los datos recopilados para rastrear y supervisar el uso de nuestro servicio. Estos datos se comparten con otros servicios de Google. Google puede utilizar los datos recopilados para contextualizar y personalizar los anuncios de su propia red publicitaria.</p>\n" +
+            "<p>Puede optar por excluirse de ciertas funciones de Google Analytics a través de la configuración de su dispositivo móvil, como la configuración de publicidad de su dispositivo o siguiendo las instrucciones proporcionadas por Google en su Política de privacidad: https://policies.google.com/privacy?hl=es</p>\n" +
+            "<p>Para obtener más información sobre las prácticas de privacidad de Google, visite la página web de Privacidad y Condiciones de Google: https://policies.google.com/privacy?hl=en</p>",
+        middle_content_rubric_14: "<strong>AppsFlyer</strong>\n" +
+            "<p>AppsFlyer es un servicio de atribución que nos permite rastrear el recorrido de un cliente hacia nuestro sitio y/o aplicación, y rastrear en qué anuncios y en qué plataforma está más interesado. Utilizando los datos que AppsFlyer recopila y potentes herramientas de análisis, podemos hacer un seguimiento de las estadísticas y el rendimiento de nuestras propias campañas publicitarias en todos los canales, tanto generales como individuales.</p>\n" +
+            "<p>Para obtener más información sobre las prácticas de privacidad de AppsFlyer, visite la página web de la Política de privacidad de AppsFlyer:</p>\n" +
+            "<span>https://www.appsflyer.com/legal/services-privacy-policy/</span>",
+        middle_content_rubric_15: "<strong>Amplitude</strong>\n" +
+            "<p>Amplitude es un sistema de análisis para nuestro producto (aplicación). Con la ayuda de sus informes, podemos analizar el comportamiento del usuario para optimizar aún más el producto.</p>\n" +
+            "<p>Para obtener más información sobre las prácticas de privacidad de Amplitude, visite la página web de aviso de privacidad de Amplitude:: https://amplitude.com/privacy</p>",
+        middle_content_rubric_16: "<strong>UXCam</strong>\n" +
+            "<p>UXCam es una plataforma de análisis digital para desarrolladores de aplicaciones, que identifica problemas de usabilidad en sus aplicaciones móviles. Recopila y procesa interacciones y recorridos completos del usuario de múltiples fuentes sin necesidad de adaptación de código.</p>\n" +
+            "<p>Para obtener más información sobre las prácticas de privacidad de UXCam, visite la página web de aviso de privacidad de UXCam: https://uxcam.com/privacy-policy</p>",
+        middle_content_rubric_17: "<p>Nuestro servicio puede contener enlaces a otros sitios que no son operados por nosotros. Si haces clic en un enlace de terceros, serás redirigido al sitio de ese tercero. Te recomendamos encarecidamente que revises la Política de Privacidad de cada sitio que visites.</p>\n" +
+            "<p>No tenemos control sobre el contenido, las políticas de privacidad o las prácticas de los sitios o servicios de terceros y no asumimos ninguna responsabilidad al respecto</p>",
+        middle_content_rubric_18: "<p>Nuestro servicio no está dirigido a ninguna persona menor de 18 años (\"Niños\").</p>\n" +
+            "<p>No recopilamos de manera consciente información personal identificable de personas menores de 18 años. Si eres padre o tutor y tienes conocimiento de que tus hijos nos han proporcionado Datos Personales, por favor contáctanos.</p>\n" +
+            "<p>Si nos damos cuenta de que hemos recopilado Datos Personales de niños sin la verificación del consentimiento parental, tomaremos medidas para eliminar esa información de nuestros servidores.</p>",
+        middle_content_rubric_19: "<p>Podemos actualizar nuestra Política de Privacidad de vez en cuando. Le notificaremos cualquier cambio publicando la nueva Política de Privacidad en esta página.</p>\n" +
+            "<p>Le informaremos por correo electrónico y/o con un aviso destacado en nuestro Servicio, antes de que el cambio entre en vigor y actualizaremos la \"fecha efectiva\" en la parte superior de esta Política de Privacidad.</p>\n" +
+            "<p>Se recomienda revisar periódicamente esta Política de Privacidad para cualquier cambio.</p>\n" +
+            "<span>Los cambios en esta Política de Privacidad son efectivos cuando se publican en esta página.</span>",
+        middle_content_rubric_20: "<div>Si tiene alguna pregunta sobre esta Política de Privacidad, contáctenos:</div>\n" +
+            "<div>Por correo electrónico: yavorsky.team@gmail.com</div>",
+        middle_content_title_1: "Recopilación y uso de información",
+        middle_content_title_2: "Tipos de datos recopilados",
+        middle_content_title_3: "Uso de datos",
+        middle_content_title_4: "Proveedores de servicios",
+        middle_content_title_5: "Enlaces a Otros Sitios",
+        middle_content_title_6: "Privacidad de los niños",
+        middle_content_title_7: "Cambios en esta Política de Privacidad",
     }
 }
 export default translate
